49776546a6e23600c008e57614db4d96d11e1c18
Avirup.Pal <Avirup.Pal@cognizant.com>
2018-12-05 21:13:53 +0530

    first commit

---
+++ src/main/java/com/orastays/booking/bookingserver/BookingServerApplication.java
@@ -0,0 +1,12 @@
+package com.orastays.booking.bookingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingServerApplication.class, args);
+	}
+}
